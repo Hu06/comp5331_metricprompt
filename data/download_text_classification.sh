52a77b6859566e30a3e4da94d541c200410945a3
Lei HAN <kshanlei@126.com>
2023-11-19 19:14:35 +0800

    update the file download part

--- data/download_text_classification.sh
+++ data/download_text_classification.sh
@@ -3,33 +3,30 @@ DIR="./TextClassification"
 mkdir $DIR
 cd $DIR
 
-
 _TRAIN_DOWNLOAD_URL="https://raw.githubusercontent.com/mhjabreel/CharCnn_Keras/master/data/ag_news_csv/train.csv"
 _TEST_DOWNLOAD_URL="https://raw.githubusercontent.com/mhjabreel/CharCnn_Keras/master/data/ag_news_csv/test.csv"
 
+rm -rf agnews
 mkdir agnews
-wget --content-disposition $_TRAIN_DOWNLOAD_URL -P agnews
-mv agnews.csv agnews/train.csv
-wget --content-disposition $_TEST_DOWNLOAD_URL -P agnews
-mv agnews.csv agnews/test.csv
-tar -
-
-
+curl $_TRAIN_DOWNLOAD_URL -o train.csv
+mv train.csv agnews/train.csv
+curl $_TEST_DOWNLOAD_URL -o test.csv
+mv test.csv agnews/test.csv
 
 rm -rf dbpedia
-
-wget --content-disposition https://s3.amazonaws.com/fast-ai-nlp/dbpedia_csv.tgz
-
+mkdir dbpedia
+curl https://s3.amazonaws.com/fast-ai-nlp/dbpedia_csv.tgz -o dbpedia_csv.tgz
 tar -zxvf dbpedia_csv.tgz
-rm -rf dbpedia_csv.tgz
-mv dbpedia_csv dbpedia
+mv dbpedia_csv/* dbpedia
+rm -rf dbpedia_csv.tgz dbpedia_csv
+
 
 rm -rf yahoo_answers_topics
-wget --content-disposition https://s3.amazonaws.com/fast-ai-nlp/yahoo_answers_csv.tgz
-
+mkdir yahoo_answers_topics
+curl  https://s3.amazonaws.com/fast-ai-nlp/yahoo_answers_csv.tgz -o yahoo_answers_csv.tgz
 tar -zxvf yahoo_answers_csv.tgz
-rm -rf yahoo_answers_csv.tgz
-mv  yahoo_answers_csv yahoo_answers_topics
+mv  yahoo_answers_csv/* yahoo_answers_topics
+rm -rf yahoo_answers_csv.tgz yahoo_answers_csv
 
 
 cd ..
